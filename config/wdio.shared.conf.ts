f52c07dc4f53d9bebac2cb0a4834509f237a79f2
Daniela-git <dhiguitaa17@gmail.com>
2024-01-31 10:36:53 -0500

    change wdio

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -66,7 +66,7 @@ export const sharedConfig: Options.Testrunner = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'error',
   //
   // Set specific log levels per logger
   // loggers:
@@ -291,24 +291,24 @@ export const sharedConfig: Options.Testrunner = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {<Object>} results object containing test results
    */
-  onComplete: function () {
-    const reportError = new Error('Could not generate Allure report');
-    const generation = allure(['generate', 'allure-results', '--clean']);
-    return new Promise<void>((resolve, reject) => {
-      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+  // onComplete: function () {
+  //   const reportError = new Error('Could not generate Allure report');
+  //   const generation = allure(['generate', 'allure-results', '--clean']);
+  //   return new Promise<void>((resolve, reject) => {
+  //     const generationTimeout = setTimeout(() => reject(reportError), 5000);
 
-      generation.on('exit', function (exitCode) {
-        clearTimeout(generationTimeout);
+  //     generation.on('exit', function (exitCode) {
+  //       clearTimeout(generationTimeout);
 
-        if (exitCode !== 0) {
-          return reject(reportError);
-        }
+  //       if (exitCode !== 0) {
+  //         return reject(reportError);
+  //       }
 
-        console.log('Allure report successfully generated');
-        resolve();
-      });
-    });
-  },
+  //       console.log('Allure report successfully generated');
+  //       resolve();
+  //     });
+  //   });
+  // },
   /**
    * Gets executed when a refresh happens.
    * @param {String} oldSessionId session ID of the old session
