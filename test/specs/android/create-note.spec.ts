e2a2fd0401f9c68bf260a7ba98c80e9d39b4da2c
Daniela-git <dhiguitaa17@gmail.com>
2024-01-31 13:55:09 -0500

    change

--- test/specs/android/create-note.spec.ts
+++ test/specs/android/create-note.spec.ts
@@ -2,6 +2,8 @@ import AddNoteScreen from '../../screenObjects/android/add-note.screen.ts';
 
 describe('create Note', () => {
   before(async () => {
+    const element = await driver.$(".//*[@text='Allow']");
+    await element.click();
     await AddNoteScreen.skipTutorial();
     await AddNoteScreen.addAndSaveNote(
       'TV shows',
